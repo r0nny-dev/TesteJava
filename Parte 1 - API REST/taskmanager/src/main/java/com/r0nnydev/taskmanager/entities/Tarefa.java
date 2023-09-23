a0a67b9b8af49940123ea370494f03a5988b5a0f
Ronald Oliveira <97353520+r0nny-dev@users.noreply.github.com>
2023-09-23 19:05:34 -0300

    Create Tarefa.java

---
+++ Parte 1 - API REST/taskmanager/src/main/java/com/r0nnydev/taskmanager/entities/Tarefa.java
@@ -0,0 +1,102 @@
+package com.r0nnydev.taskmanager.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_tarefas")
+public class Tarefa implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String titulo;
+	private String descricao;
+	private LocalDate prazo;
+	private Integer duracao;
+	private Boolean finalizado;
+
+	@ManyToOne
+	@JoinColumn(name = "idDepartamento")
+	private Departamento departamento;
+	
+	@ManyToOne(optional = true)
+	@JoinColumn(name = "idPessoa")
+	private Pessoa pessoaAlocada;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDate getPrazo() {
+		return prazo;
+	}
+
+	public void setPrazo(LocalDate prazo) {
+		this.prazo = prazo;
+	}
+
+	public Integer getDuracao() {
+		return duracao;
+	}
+
+	public void setDuracao(Integer duracao) {
+		this.duracao = duracao;
+	}
+
+	public Boolean getFinalizado() {
+		return finalizado;
+	}
+
+	public void setFinalizado(Boolean finalizado) {
+		this.finalizado = finalizado;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+	public Pessoa getPessoaAlocada() {
+		return pessoaAlocada;
+	}
+
+	public void setPessoaAlocada(Pessoa pessoaAlocada) {
+		this.pessoaAlocada = pessoaAlocada;
+	}
+	
+	
+}
