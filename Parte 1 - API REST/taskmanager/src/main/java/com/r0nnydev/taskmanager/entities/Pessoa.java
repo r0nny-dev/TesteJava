1760703f182ea95741be03d88536df9d4cc0392b
Ronald Oliveira <97353520+r0nny-dev@users.noreply.github.com>
2023-09-23 19:05:06 -0300

    Create Pessoa.java

---
+++ Parte 1 - API REST/taskmanager/src/main/java/com/r0nnydev/taskmanager/entities/Pessoa.java
@@ -0,0 +1,79 @@
+package com.r0nnydev.taskmanager.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_pessoas")
+public class Pessoa implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	
+	@ManyToOne
+	@JoinColumn(name = "idDepartamento")
+	private Departamento departamento;
+	
+	@OneToMany(mappedBy = "pessoaAlocada")
+	private List<Tarefa> tarefas = new ArrayList<>();
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+	public List<Tarefa> getTarefas() {
+		return tarefas;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(id, other.id);
+	}
+}
