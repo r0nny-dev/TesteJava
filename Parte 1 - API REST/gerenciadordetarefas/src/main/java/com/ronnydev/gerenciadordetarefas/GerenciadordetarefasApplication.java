a30529ed5c787411c5e8dce136fab8c0b47eb336
Ronald Oliveira <97353520+r0nny-dev@users.noreply.github.com>
2023-09-23 05:00:07 -0300

    Commit Inicial

---
+++ Parte 1 - API REST/gerenciadordetarefas/src/main/java/com/ronnydev/gerenciadordetarefas/GerenciadordetarefasApplication.java
@@ -0,0 +1,13 @@
+package com.ronnydev.gerenciadordetarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadordetarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadordetarefasApplication.class, args);
+	}
+
+}
